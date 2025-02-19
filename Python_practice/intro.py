7739b34e72ac81564a616c9740a5f552dd2bf698
krishnapal singh rawat <krishkprawat@gmail.com>
2025-02-19 22:02:01 +0530

    new string methods

---
+++ Python_practice/intro.py
@@ -0,0 +1,20 @@
+#Here are the some important points of the python before we start the coding..
+
+
+#string methods --
+
+name = "krishnapal singh rawat"
+print(name)
+
+print(name[0:6])
+print(name[::-1])
+print(len(name))   #include spaces
+
+print(name[0:2])
+
+print(name[:4])
+
+
+string = "ishika"
+print(string[::-2])
+print(string[-3:-1]) #this will become [6-3 : 6-1] means 3:5 which is i and k
