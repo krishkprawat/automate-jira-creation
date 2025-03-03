2e9404ba2679920b6bea19c8b898fbcd0f7d0242
krishnapal singh rawat <krishkprawat@gmail.com>
2025-03-03 21:39:15 +0530

    some other prohramming practice

--- Python_practice/loop.py
+++ Python_practice/loop.py
@@ -17,3 +17,40 @@
 #     print (k+1)
 
 
+#counting vowels in a given word --- 
+
+vowels=['a','e','i','o','u']
+string = "ishika"
+count =0
+for char in string:
+    if char in vowels:
+        count +=1
+print(count)
+
+
+#Counting the Number of Occurrences of a Character in a String
+
+
+name="ishika"
+character="k"
+count = 0
+for char in name:
+    if char==character:
+        count +=1
+print(count)  #1
+
+
+#writing a fibonaci series :
+
+a,b=0,1
+for i in range (8):
+    c = a+b
+    print(c)
+    a,b = b,c
+
+fin= [0,1]
+
+for i in range(8):
+    fin.append(fin[-1]+fin[-2])
+
+print(fin)
