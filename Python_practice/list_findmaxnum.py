0ab3f99dc48f7fc6e5dfbe38d3f08f82fc559f8b
krishnapal singh rawat <krishkprawat@gmail.com>
2025-04-26 22:37:04 +0530

    new

--- Python_practice/list_findmaxnum.py
+++ Python_practice/list_findmaxnum.py
@@ -13,3 +13,15 @@ print(max)
 listi.sort()   #list is sorted in place returns none. so dont save it in variable.
 largest= listi[-2]
 print(largest)
+
+#if list have a dulicate values then --
+
+kplist= [1,1,12,2,234,45,45345,556,66,66]
+newlist=sorted(set(kplist))
+print(newlist[-2])
+
+
+#set will give unique set result but it gives output in set so change this set to list i n last.
+nomber= [1,76,67,67,2,3,4]
+newdaru =list(set(nomber))
+print(newdaru)
